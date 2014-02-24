afc5d0e7d27d5c892fb3cda9e6c35dd2e06f0987
Maxime Hamm <mhamm@jspresso.org>
2014-02-24 19:00:49 +0000

    new action to copy jspresso table to tmar recursively

--- core/src/test/groovy/org/jspresso/hrsample/ext/backend/CompanyEmployees.groovy
+++ core/src/test/groovy/org/jspresso/hrsample/ext/backend/CompanyEmployees.groovy
@@ -1,38 +1,46 @@
 package org.jspresso.hrsample.ext.backend
 
 import org.hibernate.criterion.DetachedCriteria
-import org.jspresso.contrib.model.query.UserQuery
+import org.hibernate.criterion.Restrictions
+import org.jspresso.contrib.test.backend.tmar.TmarUtil
+import org.jspresso.framework.application.backend.BackendControllerHolder
 import org.jspresso.framework.application.backend.session.EMergeMode
 import org.jspresso.framework.model.descriptor.IComponentDescriptor
-import org.jspresso.hrsample.model.City;
+import org.jspresso.framework.model.entity.IEntity
+import org.jspresso.framework.util.accessor.IAccessor
+import org.jspresso.framework.util.accessor.IAccessorFactory
 
 class CompanyEmployees extends TmarBackendStartup {
 
-  def test() { 
+  def test() {
 
     when:
-      while (tmar.line) {
-        
-        if (tmar.currentIndexList == 0) {
-          
-          //createEntities(City, tmar.table.city)
-          
-          createEntities(
-              [Employee: tmar.table.employee,
-               Team: tmar.table.team,
-               Department: tmar.table.department,
-               City: tmar.table.city,
-               Company: tmar.table.company])
-          
-        }
-        
-        if ('COUNT' == tmar.operation) {
-          tmar.count = countTable(tmar.tableName)
-        }
-        else if ('EXISTS' == tmar.operation) {
-          tmar.exists = true
-        }
+    while (tmar.line) {
+
+      if (tmar.currentIndexList == 0) {
+
+
+        createEntities([City: tmar.table.city])
+
+        createEntities(
+            [Employee: tmar.table.employee,
+              Team: tmar.table.team,
+              Department: tmar.table.department,
+              //City: tmar.table.city,
+              Company: tmar.table.company])
+
       }
+
+      if ('COUNT' == tmar.operation) {
+        tmar.size = countTable(tmar.tableName)
+      }
+      else if ('EXISTS' == tmar.operation) {
+        tmar.exists = findEntity(tmar.tableName, tmar.keyValue as String) !=null
+      }
+      else if ('CHECK' == tmar.operation) {
+        tmar.value = findEntityField(tmar.tableName, tmar.keyValue as String, tmar.field as String)
+      }
+    }
 
     //-------
     then:
@@ -47,15 +55,62 @@ class CompanyEmployees extends TmarBackendStartup {
     where:
     tmar << getData('test')
   }
-  
-  
-  
+
+
+
   private int countTable(String clazz) {
     IComponentDescriptor<?> d = getApplicationContext().getBean(clazz);
-    Class c = d.getComponentContract(); 
-    
+    Class c = d.getComponentContract();
+
     DetachedCriteria criteria = DetachedCriteria.forClass(c);
-    List<UserQuery> queries = getBackendController().findByCriteria(criteria, EMergeMode.MERGE_KEEP, c);
+    List<?> queries = getBackendController().findByCriteria(criteria, EMergeMode.MERGE_KEEP, c);
     return queries.size();
   }
+
+  /**
+   * find entity
+   * @param clazz
+   * @param keyValue
+   * @return
+   */
+  private IEntity findEntity(String clazz, String keyValue) {
+    IComponentDescriptor<?> d = getApplicationContext().getBean(clazz as String);
+    Class c = d.getComponentContract();
+    if (keyValue.startsWith("\"")) {
+      keyValue = keyValue.substring(1, keyValue.length()-2)
+    }
+
+    String key = TmarUtil.findUnicityScope(d);
+    if (key==null) {
+      IEntity entity = getBackendController().findById(keyValue, EMergeMode.MERGE_KEEP, c)
+      return entity;
+    }
+
+    DetachedCriteria criteria = DetachedCriteria.forClass(c);
+    criteria.add(Restrictions.eq(key, keyValue));
+    IEntity entity = getBackendController().findFirstByCriteria(criteria, EMergeMode.MERGE_KEEP, c);
+    return entity;
+  }
+
+  /**
+   * find entitie's field value
+   * @param clazz
+   * @param keyValue
+   * @param field
+   * @return
+   */
+  private Object findEntityField(String clazz, String keyValue, String field) {
+    IEntity entity = findEntity(clazz, keyValue);
+    if (entity == null) {
+      return null;
+    }
+    
+    String javaField = org.jspresso.contrib.backend.tmar.TmarUtil.convertSpacesToCamelCase(field);
+    IAccessorFactory accessorFactory = BackendControllerHolder.getCurrentBackendController().getAccessorFactory();
+    IAccessor accessor = accessorFactory.createPropertyAccessor(javaField, entity.getComponentContract());
+    Object o = accessor.getValue(entity);
+//    Object o = entity.straightGetProperty(javaField)
+    return o;
+  }
+
 }
