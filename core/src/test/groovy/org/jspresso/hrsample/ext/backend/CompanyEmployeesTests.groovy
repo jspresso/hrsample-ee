cd30a4a0b73c03bd21fef51bb0b61e759f5b37af
Maxime Hamm <mhamm@jspresso.org>
2014-07-16 16:56:09 +0000

    switching to tmar for junit

---
+++ core/src/test/groovy/org/jspresso/hrsample/ext/backend/CompanyEmployeesTests.groovy
@@ -0,0 +1,436 @@
+package org.jspresso.hrsample.ext.backend
+
+import org.hibernate.criterion.DetachedCriteria
+import org.hibernate.criterion.Restrictions
+import org.jspresso.contrib.backend.tmar.TmarParameters
+import org.jspresso.contrib.frontend.ImportExportUtils;
+import org.jspresso.contrib.test.backend.tmar.TmarPlus
+import org.jspresso.framework.application.backend.BackendControllerHolder
+import org.jspresso.framework.application.backend.session.EMergeMode
+import org.jspresso.framework.model.descriptor.IComponentDescriptor
+import org.jspresso.framework.model.entity.IEntity
+import org.jspresso.framework.util.accessor.IAccessor
+import org.jspresso.framework.util.accessor.IAccessorFactory
+import org.jspresso.framework.util.uid.ByteArray
+import org.jspresso.hrsample.model.City;
+import org.jspresso.hrsample.model.Company;
+import org.jspresso.hrsample.model.Department;
+import org.jspresso.hrsample.model.Employee;
+import org.jspresso.hrsample.model.Team;
+import org.junit.After;
+import org.junit.Test;
+
+class CompanyEmployeesTests extends TmarBackendStartup {
+
+  /**
+   * test1
+   */
+  @Test
+  void test1() {
+    
+    eachIterationWithIndex('test1') { tmar, iterationNumber ->
+      
+      if (iterationNumber == 1) {
+        createEntities(
+            [City: tmar.table.city])
+        createEntities(
+            [Employee: tmar.table.employee,
+              Team: tmar.table.team,
+              Department: tmar.table.department,
+              Company: tmar.table.company])
+      }
+      
+      prepareThen(tmar)
+    }
+  }
+
+
+  /**
+   * test2
+   */
+  @Test
+  void test2() {
+    
+    eachIterationWithIndex('test2') { tmar, iterationNumber ->
+      
+      if (iterationNumber == 1) {
+        createEntities([City: tmar.table.city])
+
+        createEntities([Company: tmar.table.company])
+
+        createEntities(
+            [Employee: tmar.table.employee,
+              Team: tmar.table.team,
+              Department: tmar.table.department])
+      }
+      
+      prepareThen(tmar)
+    }
+    
+//    when:
+//    while (tmar.hasNext()) {
+//      if (tmar.currentIndexList == 0) {
+//        createEntities([City: tmar.table.city])
+//
+//        createEntities([Company: tmar.table.company])
+//
+//        createEntities(
+//            [Employee: tmar.table.employee,
+//              Team: tmar.table.team,
+//              Department: tmar.table.department])
+//      }
+//      prepareThen(tmar)
+//    }
+//
+//    //-------
+//    then:
+//    tmar.asserts()
+//
+//    //-------
+//    cleanup:
+//    cleanupData(tmar);
+//
+//    //-------
+//    where:
+//    tmar << getData('test2')
+
+  }
+
+
+
+  /**
+   * test3
+   */
+  @Test
+  void test3() {
+
+    // remove all tmar ids !
+    getTmarParameters().setTmarIds(null)
+    
+    eachIterationWithIndex('test3') { tmar, iterationNumber ->
+      
+      if (iterationNumber == 1) {
+        createEntities([City: tmar.table.city])
+
+        createEntities([Company: tmar.table.company])
+
+        createEntities(
+            [Employee: tmar.table.employee,
+              Team: tmar.table.team,
+              Department: tmar.table.department])
+      }
+      
+      prepareThen(tmar)
+    }
+    
+//    when:
+//
+//    // remove all tmar ids !
+//    getTmarParameters().setTmarIds(null)
+//
+//    while (tmar.hasNext()) {
+//      if (tmar.currentIndexList == 0) {
+//        createEntities([City: tmar.table.city])
+//
+//        createEntities([Company: tmar.table.company])
+//
+//        createEntities(
+//            [Employee: tmar.table.employee,
+//              Team: tmar.table.team,
+//              Department: tmar.table.department])
+//      }
+//      prepareThen(tmar)
+//    }
+//
+//    //-------
+//    then:
+//    tmar.asserts()
+//
+//    //-------
+//    cleanup:
+//    cleanupData(tmar);
+//
+//    //-------
+//    where:
+//    tmar << getData('test3')
+
+  }
+
+
+  /**
+   * test4
+   */
+  @Test
+  void test4() {
+    
+    eachIterationWithIndex('test4') { tmar, iterationNumber ->
+      
+      if (iterationNumber == 1) {
+        createEntities(
+          Company: tmar.table.company)
+            
+        // companies has to be saved before, because of use 
+        // of reference "company.name" which will trigger a
+        // database access
+        createEntities(
+          Employee: tmar.table.employee)
+        
+        // employees has to be saved before, because of use 
+        // of multi field reference "manager.name" + "manager.first name"
+        // which will trigger a database access
+        createEntities(
+           Department: tmar.table.department)
+      }
+      
+      prepareThen(tmar)
+    }
+
+//    when:
+//    while (tmar.hasNext()) {
+//      if (tmar.currentIndexList == 0) {
+//        
+//        createEntities(
+//          Company: tmar.table.company)
+//            
+//        // companies has to be saved before, because of use 
+//        // of reference "company.name" which will trigger a
+//        // database access
+//        createEntities(
+//          Employee: tmar.table.employee)
+//        
+//        // employees has to be saved before, because of use 
+//        // of multi field reference "manager.name" + "manager.first name"
+//        // which will trigger a database access
+//        createEntities(
+//           Department: tmar.table.department)
+//      }
+//      prepareThen(tmar)
+//    }
+//
+//    //-------
+//    then:
+//    tmar.asserts()
+//
+//    //-------
+//    cleanup:
+//    cleanupData(tmar);
+//
+//    //-------
+//    where:
+//    tmar << getData('test4')
+
+  }
+
+
+  /**
+   * test5
+   */
+  @Test
+  void test5() {
+
+    eachIterationWithIndex('test5') { tmar, iterationNumber ->
+      
+      if (iterationNumber == 1) {
+        createEntities(
+          Company: tmar.table.company)
+            
+        // companies has to be saved before, because of use
+        // of reference "company.name" which will trigger a
+        // database access
+        createEntities(
+          Employee: tmar.table.employee)
+        
+        // employees has to be saved before, because of use
+        // of multi field reference "manager.name" + "manager.first name"
+        // which will trigger a database access
+        createEntities(
+           Department: tmar.table.department)
+      }
+      
+      prepareThen(tmar)
+    }
+    
+//    when:
+//    while (tmar.hasNext()) {
+//      if (tmar.currentIndexList == 0) {
+//        
+//        createEntities(
+//          Company: tmar.table.company)
+//            
+//        // companies has to be saved before, because of use
+//        // of reference "company.name" which will trigger a
+//        // database access
+//        createEntities(
+//          Employee: tmar.table.employee)
+//        
+//        // employees has to be saved before, because of use
+//        // of multi field reference "manager.name" + "manager.first name"
+//        // which will trigger a database access
+//        createEntities(
+//           Department: tmar.table.department)
+//      }
+//      prepareThen(tmar)
+//    }
+//
+//    //-------
+//    then:
+//    tmar.asserts()
+//
+//    //-------
+//    cleanup:
+//    cleanupData(tmar);
+//
+//    //-------
+//    where:
+//    tmar << getData('test5')
+
+  }
+  
+  
+  
+
+  /**
+   * cleanup data
+   * @param tmar
+   * @return
+   */
+  @After
+  public void cleanupData() {
+    deleteAllEntities(Company.class)
+    deleteAllEntities(City.class)
+  }
+
+  /**
+   * prepate then
+   * @param tmar
+   * @return
+   */
+  private prepareThen(tmar) {
+    if ('COUNT' == tmar.operation) {
+      tmar.size = countTable(tmar.tableName)
+    }
+    else if ('EXISTS' == tmar.operation) {
+      tmar.exists = findEntity(tmar.tableName, tmar.keyValue as String) !=null
+    }
+    else if ('CHECK' == tmar.operation) {
+      tmar.value = findEntityField(tmar.tableName, tmar.keyValue as String, tmar.field as String)
+    }
+    return tmar
+  }
+
+
+
+
+
+  //  /**
+  //   * do test
+  //   */
+  //  def doTest(Closure createEntitiesClosure) {
+  //
+  //    when:
+  //    while (tmar.hasNext()) {
+  //
+  //      if (tmar.currentIndexList == 0) {
+  //        createEntitiesClosure.call()
+  //      }
+  //
+  //      if ('COUNT' == tmar.operation) {
+  //        tmar.size = countTable(tmar.tableName)
+  //      }
+  //      else if ('EXISTS' == tmar.operation) {
+  //        tmar.exists = findEntity(tmar.tableName, tmar.keyValue as String) !=null
+  //      }
+  //      else if ('CHECK' == tmar.operation) {
+  //        tmar.value = findEntityField(tmar.tableName, tmar.keyValue as String, tmar.field as String)
+  //      }
+  //    }
+  //
+  //    //-------
+  //    then:
+  //    tmar.asserts()
+  //
+  //    //-------
+  //    where:
+  //    tmar << getData('test')
+  //  }
+
+
+
+  /**
+   * countTable
+   * @param clazz
+   * @return
+   */
+  private int countTable(String clazz) {
+    IComponentDescriptor<?> d = getApplicationContext().getBean(clazz);
+    Class c = d.getComponentContract();
+
+    DetachedCriteria criteria = DetachedCriteria.forClass(c);
+    List<?> queries = getBackendController().findByCriteria(criteria, EMergeMode.MERGE_KEEP, c);
+
+    return queries.size();
+  }
+
+  /**
+   * find entity
+   * @param clazz
+   * @param keyValue
+   * @return
+   */
+  private IEntity findEntity(String clazz, String keyValue) {
+    IComponentDescriptor<?> d = getApplicationContext().getBean(clazz);
+    Class c = d.getComponentContract();
+    if (keyValue.startsWith("\"")) {
+      keyValue = keyValue.substring(1, keyValue.length()-2)
+    }
+
+    String key = null;
+    TmarParameters param = getTmarParameters();
+    if (param.getTmarIds()!=null) {
+      key = param.getTmarIds().get(clazz);
+    }
+
+    if (key == null) {
+      key = ImportExportUtils.getUnicityScopeColumn(d, param.getContext());
+    }
+
+    if ("id".equals(key)) {
+      Serializable keyId = isStringGUIDGenerator() ? keyValue : new ByteArray(keyValue.getBytes())
+
+      IEntity entity = getBackendController().findById(keyId, EMergeMode.MERGE_KEEP, c)
+      return entity;
+    }
+    else {
+      DetachedCriteria criteria = DetachedCriteria.forClass(c);
+      criteria.add(Restrictions.eq(key, keyValue));
+      IEntity entity = getBackendController().findFirstByCriteria(criteria, EMergeMode.MERGE_KEEP, c);
+      return entity;
+    }
+  }
+
+  /**
+   * find entitie's field value
+   * @param clazz
+   * @param keyValue
+   * @param field
+   * @return
+   */
+  private Object findEntityField(String clazz, String keyValue, String field) {
+    IEntity entity = findEntity(clazz, keyValue);
+    if (entity == null) {
+      return null;
+    }
+
+    String javaField = org.jspresso.contrib.backend.tmar.TmarUtil.convertSpacesToCamelCase(field);
+    IAccessorFactory accessorFactory = BackendControllerHolder.getCurrentBackendController().getAccessorFactory();
+    IAccessor accessor = accessorFactory.createPropertyAccessor(javaField, entity.getComponentContract());
+    Object o = accessor.getValue(entity);
+
+    return o;
+  }
+
+
+
+
+
+}
--- core/src/test/groovy/org/jspresso/hrsample/ext/backend/TmarBackendStartup.groovy
+++ core/src/test/groovy/org/jspresso/hrsample/ext/backend/TmarBackendStartup.groovy
@@ -1,9 +1,13 @@
-package org.jspresso.hrsample.ext.backend
+package org.jspresso.hrsample.ext.backend;
+
+import java.security.acl.Group
+
+import javax.security.auth.Subject
 
 import org.hibernate.criterion.DetachedCriteria
 import org.jboss.security.SimpleGroup
 import org.jboss.security.SimplePrincipal
-import org.jspresso.contrib.test.application.startup.AbstractTmarBackendStartup
+import org.jspresso.contrib.test.application.startup.AbstractTmar4JUnitBackendStartup
 import org.jspresso.framework.application.backend.persistence.hibernate.HibernateBackendController
 import org.jspresso.framework.model.entity.IEntity
 import org.jspresso.framework.model.persistence.hibernate.criterion.EnhancedDetachedCriteria
@@ -12,11 +16,12 @@ import org.jspresso.framework.security.UserPrincipal
 import org.jspresso.framework.util.exception.NestedRuntimeException
 import org.jspresso.hrsample.model.City
 import org.jspresso.hrsample.model.Company
+import org.junit.After
+import org.junit.AfterClass
+import org.junit.Before
 import org.springframework.transaction.TransactionStatus
 import org.springframework.transaction.support.TransactionCallbackWithoutResult
 
-import javax.security.auth.Subject
-import java.security.acl.Group
 
 /**
  * Base class for integration tests.
@@ -24,28 +29,14 @@ import java.security.acl.Group
  * @version $LastChangedRevision$ 
  * @author Maxime Hamm
  */
-public class TmarBackendStartup extends AbstractTmarBackendStartup {
-
-  /**
-   * Performs DB initialization and test data creation. 
-   * 
-   * @throws java.lang.Exception
-   */
-  //@BeforeClass
-  //static setUpBeforeClass
-  public void setupSpec() throws Exception {
-    //TmarBackendStartup startup = new TmarBackendStartup();
-    //new TestDataPersister(startup.getApplicationContext()).persistTestData();
-  }
+public class TmarBackendStartup extends AbstractTmar4JUnitBackendStartup {
 
   /**
    * Destroys all data from DB.
-   * 
    * @throws java.lang.Exception
    */
-  //@AfterClass
-  //static tearDownAfterClass
-  public void cleanupSpec() throws Exception {
+  @AfterClass
+  public static void cleanupSpec() throws Exception {
     
     TmarBackendStartup startup = new TmarBackendStartup();
     startup.start();
@@ -65,8 +56,7 @@ public class TmarBackendStartup extends AbstractTmarBackendStartup {
    * 
    * @throws java.lang.Exception
    */
-  //@Before
-  //setUp
+  @Before
   public void setup() throws Exception {
     start();
     configureApplicationSession(createTestSubject(), getStartupLocale());
@@ -77,8 +67,7 @@ public class TmarBackendStartup extends AbstractTmarBackendStartup {
    *
    * @throws java.lang.Exception
    */
-  //@After
-  //tearDown
+  @After
   public void cleanup() throws Exception {
     getBackendController().cleanupRequestResources();
     getBackendController().stop();
