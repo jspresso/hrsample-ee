578bd1bacf3a7af33acd13b87f2af719370d3625
Maxime HAMM <maxime.hamm@laposte.net>
2016-01-28 11:30:42 +0100

    tune skin (as for hrsample)

--- startup/qooxdoo/client/src/main/js/org/jspresso/hrsample/ext/startup/qooxdoo/theme/Appearance.js
+++ startup/qooxdoo/client/src/main/js/org/jspresso/hrsample/ext/startup/qooxdoo/theme/Appearance.js
@@ -14,5 +14,25 @@ qx.Theme.define("org.jspresso.hrsample.ext.startup.qooxdoo.theme.Appearance",
 
   appearances :
   {
+    "workspace-tree-folder/icon": {
+      base: true,
+      style: function (states) {
+        return {
+          height: 0,
+          width:0,
+          margin: [30, 0, 0, 20]
+        };
+      }
+    },
+
+    "accordion-section/bar": {
+      base: true,
+      style: function (states) {
+        return {
+          show: "label"
+        };
+      }
+    }
+
   }
 });
