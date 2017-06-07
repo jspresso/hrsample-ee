dbfe4aadc3e5a7c16f16bd3409663ab41b42414e
Vincent Vandenschrick <vvandens@gmail.com>
2017-06-07 08:35:51 +0200

    Jspresso extensions new signature for imports

--- core/src/main/java/org/jspresso/hrsample/ext/frontend/ImportEmployeeFactoryBean.java
+++ core/src/main/java/org/jspresso/hrsample/ext/frontend/ImportEmployeeFactoryBean.java
@@ -22,10 +22,11 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import java.util.Map;
 
-import org.jspresso.contrib.frontend.ImportEntitiesFactoryBean;
-import org.jspresso.contrib.frontend.ImportReference;
 import org.jspresso.framework.action.ActionBusinessException;
 import org.jspresso.framework.model.entity.IEntity;
+
+import org.jspresso.contrib.frontend.ImportEntitiesFactoryBean;
+import org.jspresso.contrib.frontend.ImportReference;
 import org.jspresso.hrsample.model.City;
 import org.jspresso.hrsample.model.Company;
 import org.jspresso.hrsample.model.ContactInfo;
@@ -34,9 +35,10 @@ import org.jspresso.hrsample.model.Employee;
 /**
  * Import Employees
  * Automatically create Company and City if does not exists
+ *
  * @author Maxime HAMM
  */
-public class ImportEmployeeFactoryBean extends ImportEntitiesFactoryBean  {
+public class ImportEmployeeFactoryBean extends ImportEntitiesFactoryBean {
 
   /**
    * ImportEmployeeFactoryBean
@@ -48,7 +50,7 @@ public class ImportEmployeeFactoryBean extends ImportEntitiesFactoryBean  {
    * ZIP
    */
   public static final String ZIP = "zip";
-  
+
   /**
    * CONTACT_CITY
    */
@@ -58,7 +60,8 @@ public class ImportEmployeeFactoryBean extends ImportEntitiesFactoryBean  {
    * create Company and City if does not exists
    */
   @Override
-  public IEntity findReference(ImportReference refData, Map<String, String> cells, Map<String, Object> context) throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+  public IEntity findReference(ImportReference refData, Map<String, String> cells, Map<String, Object> context)
+      throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
 
     // load from database
     IEntity entity = super.findReference(refData, cells, context);
@@ -72,8 +75,7 @@ public class ImportEmployeeFactoryBean extends ImportEntitiesFactoryBean  {
         // manage cache !
         getCache(context).put(refData.getKeyCache(), entity);
       }
-    }
-    else if (CONTACT_CITY.equals(property)) {
+    } else if (CONTACT_CITY.equals(property)) {
       if (entity == null) {
         entity = getBackendController().getEntityFactory().createEntityInstance(City.class);
         ((City) entity).setName(cells.get(CONTACT_CITY));
@@ -95,9 +97,10 @@ public class ImportEmployeeFactoryBean extends ImportEntitiesFactoryBean  {
    * manage also an extrat column 'test'
    */
   @Override
-  protected void checkLineFormat(List<String> columns, List<String> cells) {
+  protected void checkLineFormat(List<String> columns, List<String> cells, String line, int idx) {
     if (columns.size() + 1 != cells.size()) {
-      throw new ActionBusinessException("Wrong file format for import", "import.file.formatError");
+      throw new ActionBusinessException("Wrong file format for import", "import.file.formatError", idx - 1,
+          cells.size(), columns.size(), line);
     }
   }
 }
