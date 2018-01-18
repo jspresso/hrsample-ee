bc72a9d5dc8daa26b181ac8ab6bd4b2b42bafb08
Vincent Vandenschrick <vvandens@gmail.com>
2018-01-18 09:51:42 +0100

    Tests fixed

--- core/src/test/groovy/org/jspresso/hrsample/ext/backend/PivotAdminDynamicStyles.groovy
+++ core/src/test/groovy/org/jspresso/hrsample/ext/backend/PivotAdminDynamicStyles.groovy
@@ -1,25 +1,12 @@
 package org.jspresso.hrsample.ext.backend
 
-import org.hibernate.criterion.DetachedCriteria
-import org.hibernate.criterion.Restrictions
 import org.jspresso.contrib.model.pivot.PivotSetup
-import org.jspresso.contrib.model.pivot.PivotSetupField
-import org.jspresso.contrib.model.pivot.PivotStyleSet
-import org.jspresso.contrib.model.pivot.service.PivotSetupServiceDelegate
 import org.jspresso.framework.application.backend.BackendControllerHolder
 import org.jspresso.framework.application.backend.persistence.hibernate.HibernateBackendController
-import org.jspresso.framework.application.backend.session.EMergeMode
-import org.jspresso.framework.application.model.Module
-import org.jspresso.framework.application.model.Workspace
 import org.jspresso.framework.ext.pivot.backend.PivotHelper
-import org.jspresso.framework.ext.pivot.model.IStyle
 import org.jspresso.framework.ext.pivot.model.PivotFilterableBeanCollectionModule
-import org.jspresso.framework.model.entity.IEntity
-import org.jspresso.framework.model.persistence.hibernate.criterion.EnhancedDetachedCriteria;
-import org.junit.After;
+import org.junit.After
 import org.junit.Test
-import org.springframework.transaction.TransactionStatus
-import org.springframework.transaction.support.TransactionCallbackWithoutResult
 
 class PivotAdminDynamicStyles extends PivotAdmin {
 
