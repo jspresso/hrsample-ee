88861b5cdfb26df664e7fc7b30543d64f40b2c28
Vincent Vandenschrick <vvandens@gmail.com>
2019-01-23 17:31:09 +0100

    evolution : QX Debug clients

--- startup/qooxdoo/client/src/main/js/org/jspresso/hrsample/ext/startup/qooxdoo/Application.js
+++ startup/qooxdoo/client/src/main/js/org/jspresso/hrsample/ext/startup/qooxdoo/Application.js
@@ -32,10 +32,10 @@ qx.Class.define("org.jspresso.hrsample.ext.startup.qooxdoo.Application",
       var remoteController;
       if (qx.core.Environment.get("qx.debug")) {
         remoteController = new org.jspresso.framework.io.Rpc(
-            "http://localhost:8080/hrsample-ext-webapp/.qxrpc",
+            "http://localhost:9080/hrsample-ext-webapp/.qxrpc",
             "org.jspresso.hrsample.ext.startup.remote.RemoteApplicationStartup"
         );
-        remoteController.setCrossDomain(true);
+        //remoteController.setCrossDomain(true);
       } else {
         remoteController = new org.jspresso.framework.io.Rpc(
             org.jspresso.framework.io.Rpc.makeServerURL(),
